18f27a945307d46496689b4db5e2042cde25ceb8
MoussaGRICHE <grimoussa@gmail.com>
2023-05-05 10:37:34 +0200

    add docs

--- level3.py
+++ level3.py
@@ -6,28 +6,30 @@ num_segments = int(input("Enter the number of segments: "))
 
 distances = []
 steering_angles = []
-
+"""
 for i in range(num_segments):
     distances.append(float(input(f"Enter the distance for segment {i+1} (in meters): ")))
     steering_angles.append(float(input(f"Enter the steering angle for segment {i+1} (in degrees): ")))
-
+"""
 x = 0
 y = 0
 new_direction = 0
 
 for i in range(num_segments):
-    distance = distances[i]
-    steering_angle = steering_angles[i]
+    distance = float(input(f"Enter the distance for segment {i+1} (in meters): "))
+    steering_angle = float(input(f"Enter the steering angle for segment {i+1} (in degrees): "))
 
     if steering_angle == 0.00:
         x += distance * math.sin(math.radians(new_direction))
         y += distance * math.cos(math.radians(new_direction))
         new_direction = new_direction
+        print (round(x, 2),round(y, 2),round(new_direction % 360, 2))
 
     elif steering_angle == 90.00:
         x += distance * math.cos(math.radians(new_direction))
         y += distance * math.sin(math.radians(new_direction))
         new_direction = new_direction
+        print (round(x, 2),round(y, 2),round(new_direction % 360, 2))
 
     else:
         steering_angle_rad = math.radians(steering_angle)
@@ -43,7 +45,7 @@ for i in range(num_segments):
         x += xx * math.cos(theta_rad) + yy * math.sin(theta_rad)
         y += - xx * math.sin(theta_rad) + yy * math.cos(theta_rad)
         new_direction += direction
-        print (x,y,new_direction)
+        print (round(x, 2),round(y, 2),round(new_direction % 360, 2))
 
 x = round(x, 2)
 y = round(y, 2)
